be44665411733aa727ca81b9982840b25f5e9865
Eugene Skrobov <skrobov.ev@gmail.com>
2025-04-27 18:11:11 +0700

    test(GStoreOptions): improve lazy initialize tests with useReact call

--- lib/store.options.test.tsx
+++ lib/store.options.test.tsx
@@ -1,3 +1,4 @@
+import { render } from '@testing-library/react';
 import { describe, test, expect, vi } from 'vitest';
 import { GStore } from './store.ts';
 
@@ -123,21 +124,81 @@ describe('GStore - GStoreOptions', () => {
     });
   });
 
+  describe('onSubscribed + onFirstSubscribed + onUnsubscribed + onAllUnsubscribed', () => {
+    test('should not affect together', () => {
+      const onSubscribed = vi.fn();
+      const onUnsubscribed = vi.fn();
+      const onAllUnsubscribed = vi.fn();
+      const onFirstSubscribed = vi.fn();
+
+      const gStore = new GStore(stateFactoryStub, {
+        onSubscribed,
+        onUnsubscribed,
+        onAllUnsubscribed,
+        onFirstSubscribed,
+      });
+
+      const unsubscribeFirst = gStore.subscribe(() => ({}));
+
+      expect(onFirstSubscribed).toHaveBeenCalledOnce();
+
+      const unsubscribeSecond = gStore.subscribe(() => ({}));
+      const unsubscribeThird = gStore.subscribe(() => ({}));
+
+      expect(onSubscribed).toHaveBeenCalledTimes(3);
+
+      unsubscribeFirst();
+      unsubscribeSecond();
+
+      expect(onAllUnsubscribed).not.toHaveBeenCalled();
+
+      unsubscribeThird();
+
+      expect(onFirstSubscribed).toHaveBeenCalledOnce();
+
+      expect(onSubscribed).toHaveBeenCalledTimes(3);
+      expect(onSubscribed).toHaveBeenLastCalledWith(3);
+
+      expect(onUnsubscribed).toHaveBeenLastCalledWith(0);
+      expect(onUnsubscribed).toHaveBeenCalledTimes(3);
+
+      expect(onAllUnsubscribed).toHaveBeenCalledOnce();
+    });
+  });
+
   describe('initialize option', () => {
-    test('does not call initialize with "lazy"', () => {
+    test('does not call initialize with "lazy" before useReact call', () => {
       const initialize = vi.spyOn(GStore.prototype, 'initialize');
 
-      new GStore(stateFactoryStub, { initialize: 'lazy' });
+      const gStore = new GStore(stateFactoryStub, { initialize: 'lazy' });
 
       expect(initialize).not.toHaveBeenCalled();
+
+      const TestComponent = () => {
+        gStore.useReact(() => null);
+        return null;
+      };
+
+      render(<TestComponent />);
+
+      expect(initialize).toHaveBeenCalledOnce();
     });
 
-    test('does not call initialize by default', () => {
+    test('"lazy" initialize by default', () => {
       const initialize = vi.spyOn(GStore.prototype, 'initialize');
 
-      new GStore(stateFactoryStub);
+      const gStore = new GStore(stateFactoryStub);
 
       expect(initialize).not.toHaveBeenCalled();
+
+      const TestComponent = () => {
+        gStore.useReact(() => null);
+        return null;
+      };
+
+      render(<TestComponent />);
+
+      expect(initialize).toHaveBeenCalledOnce();
     });
 
     test('calls initialize with "eager" when store is created', () => {
